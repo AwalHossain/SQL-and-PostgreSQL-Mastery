419a0c66cc9d87d237f12e0fd6c7266a219b3d0b
Awal hossain <waqin21@gmail.com>
2022-10-01 13:33:29 +0600

    calculate column value 🧮

---
+++ calculateColumn.sql
@@ -0,0 +1,4 @@
+
+-- To get the how many people live in a square kilometer, we can dived the population by area which will give us the density per sqkm
+
+select name, population/area as density from cities
