a5eb6114a0e40a7e18d77b8ea8934414764e9399
Awal hossain <waqin21@gmail.com>
2022-10-01 14:21:07 +0600

    string operator plus function use in a query 🎈

--- string_operator_&_Funciton.sql
+++ string_operator_&_Funciton.sql
@@ -1,4 +1,7 @@
--- To merget two or multiple string together, we can use some string operator as well function
+-- To merge two or multiple string together, we can use some string operator as well function
 
 
-select name || country from citites;
+select name || ', ' || country from citites;  /* Mergign two or multiple string using pipe only works in postgreSQL or might be SQL but not supported in MySQL */
+
+
+select upper(concat(name, ', ' , country)) as location  from cities; /* Works fine in MysQL and PostgreSQL */
