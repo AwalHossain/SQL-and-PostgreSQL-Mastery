537a6731b48dab28bb1e13297f9b270e64913856
Awal hossain <waqin21@gmail.com>
2022-11-04 16:41:25 +0600

    two query merge by union 🔥

--- unions & interaction with sets/union.sql
+++ unions & interaction with sets/union.sql
@@ -1,3 +1,13 @@
 -- Find the 4 products with the highest price and 4 products with the highest price/weight ratio
 
 
+(SELECT *
+FROM products
+ORDER BY price DESC 
+LIMIT 4)
+UNION ALL
+(SELECT * FROM products 
+ORDER BY price/weight DESC
+LIMIT 4)
+
+
