f9071c305e936a2b963696428388ff718acb270e
Awal hossain <waqin21@gmail.com>
2022-11-06 19:33:57 +0600

    sub-query 😉

---
+++ sub-query/subQuery.sql
@@ -0,0 +1,11 @@
+-- List the name and price of all products that are more expensive than all products in the toys department
+
+-- query with parenthesis() indicate sub-query
+
+SELECT name, price 
+FROM products
+WHERE price >(
+    SELECT MAX(price)
+    FROM products 
+    WHERE department = 'Toys'
+);
