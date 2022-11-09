35b1184d6df893a6a0167f96a742758c950b5857
Awal hossain <waqin21@gmail.com>
2022-11-09 14:43:52 +0600

    select without from keyword 🥕

---
+++ sub-query/practise-2.sql
@@ -0,0 +1,13 @@
+--  using only sub-queries, print max price, min price, and average price of all phones
+
+
+SELECT 
+    (
+        SELECT MAX(price) FROM phones
+    ) as max_price,
+    (
+         SELECT MIN(price) FROM phones
+    ) as min_price,
+    (
+         SELECT avg(price) FROM phones
+    ) as avg,
