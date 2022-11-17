4d3c2dbee7e908b2eda6e44c18f318cdb4afcfe8
Awal hossain <waqin21@gmail.com>
2022-11-17 10:07:56 +0600

    use of greatest keyword to get max value 😊

--- utility_operators/greatest_keyword.sql
+++ utility_operators/greatest_keyword.sql
@@ -1,2 +1,6 @@
 
--- greatest keyword helps you to find 
+-- greatest keyword helps you to find the max val out of different values
+-- if the weight multiple two is less than 30, then 
+
+SELECT GREATEST(30, 2*weight)
+FROM products
