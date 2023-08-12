ab765bfd9ad4ddd87cf72005807fde8d0c24b506
AwalHo <74599417+AwalHo@users.noreply.github.com>
2023-08-12 02:26:22 +0000

    second practise task is done ✅

--- practise-task/index.sql
+++ practise-task/index.sql
@@ -1,3 +1,23 @@
 
 SELECT emp_name, salary, department FROM employees 
 WHERE department = 'HR';
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id INT,
+    total_amount DECIMAL(10, 2)
+);
+
+INSERT INTO orders (order_id, customer_id, total_amount)
+VALUES
+    (101, 1, 200.00),
+    (102, 2, 300.00),
+    (103, 1, 150.00),
+    (104, 3, 400.00),
+    (105, 2, 250.00);
+
+
+SELECT customer_id, avg(total_amount) as avg_total  FROM orders
+GROUP BY(customer_id) HAVING avg(total_amount) >250;
+
+
