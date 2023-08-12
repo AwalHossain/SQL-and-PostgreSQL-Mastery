558b592412d8fde0b60327f4fa4ed4f7c436ec92
AwalHo <74599417+AwalHo@users.noreply.github.com>
2023-08-12 02:54:54 +0000

    third task completed 🐱

--- practise-task/index.sql
+++ practise-task/index.sql
@@ -21,3 +21,48 @@ SELECT customer_id, avg(total_amount) as avg_total  FROM orders
 GROUP BY(customer_id) HAVING avg(total_amount) >250;
 
 
+CREATE TABLE students (
+    student_id INT PRIMARY KEY,
+    student_name VARCHAR(50),
+    age INT,
+    gender VARCHAR(10)
+);
+
+CREATE TABLE courses (
+    course_id INT PRIMARY KEY,
+    course_name VARCHAR(50),
+    credits INT
+);
+
+INSERT INTO students (student_id, student_name, age, gender)
+VALUES
+    (1, 'Alice', 22, 'Female'),
+    (2, 'Bob', 21, 'Male'),
+    (3, 'Charlie', 23, 'Male');
+
+INSERT INTO courses (course_id, course_name, credits)
+VALUES
+    (101, 'Mathematics', 3),
+    (102, 'History', 4),
+    (103, 'Physics', 3);
+
+-- Enrollment table with student-course relationships
+CREATE TABLE enrollment (
+    enrollment_id INT PRIMARY KEY,
+    student_id INT,
+    course_id INT
+);
+
+INSERT INTO enrollment (enrollment_id, student_id, course_id)
+VALUES
+    (1, 1, 101),
+    (2, 1, 102),
+    (3, 2, 103),
+    (4, 3, 101);
+
+
+
+SELECT s.student_name, c.course_name, c.credits from enrollment e
+ join students s on s.student_id = e.student_id 
+ JOIN courses c on c.course_id = e.course_id;
+ 
