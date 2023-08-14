046755a224341781b3bed933ed304efa276ca666
AwalHossain <182204.te@student.just.edu.bd>
2023-08-14 02:02:39 +0000

    user service 🚀

---
+++ Prisma-project/src/modules/user/user.service.ts
@@ -0,0 +1,62 @@
+import { PrismaClient, User } from "@prisma/client";
+
+
+const prisma = new PrismaClient();
+
+const insertIntoDB = async (data: User): Promise<User> => {
+
+    const result = await prisma.user.create(
+        {
+            data
+        }
+    )
+
+    return result;
+
+}
+
+
+const insertOrUpdateIntoDB = async (data: User): Promise<User> => {
+
+    const result = await prisma.user.upsert(
+        {
+            where: {
+                id: data.id
+            },
+            update: data,
+            create: data
+        }
+    )
+
+    return result;
+
+}
+
+
+const getUsers = async (): Promise<User[]> => {
+    const result = await prisma.user.findMany(
+        {
+            include: {
+                Profile: true
+            }
+        },
+
+    );
+    return result;
+}
+
+
+const getSingleUser = async (id: number): Promise<User | null> => {
+
+    const result = await prisma.user.findUnique(
+        {
+            where: {
+                id
+            },
+            include: {
+                Profile: true
+            }
+        }
+    )
+    return result;
+}
